746b7db4c2f9243d2d5e70048329580eb6ec0cfb
Thorhallur O <thorhallur30102004@gmail.com>
2024-08-13 13:45:18 +0000

    fixed release script not zipping

--- scripts/linux/release.sh
+++ scripts/linux/release.sh
@@ -1,6 +1,6 @@
 buildPath=build/$1
-mkdir $buildPath
-. scripts/compile.sh
+mkdir -p $buildPath
+. scripts/linux/compile.sh
 cp out/WarsimLauncher.exe $buildPath/WarsimLauncher.exe
 cp README.md $buildPath/README.md
-zip -r -b release/WLv$1.zip "$buildPath/README.md" "$buildPath/WarsimLauncher.exe"
+zip -r -j release/$1.zip $buildPath/*
